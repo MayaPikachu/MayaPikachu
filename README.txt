6c0e2b6180b0f64817b65e4d52645472940e0d9f
mdco-ld <migueldco14@gmail.com>
2023-11-02 21:27:08 -0300

    234tree -> 234tree.py

---
+++ README.txt
@@ -0,0 +1,18 @@
+# Compilando
+
+use o comando
+```
+g++ trab_4.cpp -o trab_4.exe
+```
+para compilar o programa.
+
+
+# Executando
+
+Para executar o programa, use `./trab_4.exe caminho/pro/arquivo`.
+
+
+# Resultados 
+
+Os resultados são escritos nos arquivos "Info_ABB.txt" e "Info_AVL.txt".
+As 10 palavras mais frequentes são escritas para a saída padrão.
---
+++ a.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+echo '--------------------------'
+echo 'mam-na-np-lo.txt'
+echo '--------------------------'
+./trab_4 ./mam-na-np-lo.txt && cat Info_A*
+echo '--------------------------'
+echo ''
+
+echo '--------------------------'
+echo 'exame_10000-na-np-lo.txt'
+echo '--------------------------'
+./trab_4 ./exame_10000-na-np-lo.txt && cat Info_A*
+echo '--------------------------'
+echo ''
+
+echo '--------------------------'
+echo 'biblia-na-np-lo.txt'
+echo '--------------------------'
+./trab_4 ./biblia-na-np-lo.txt && cat Info_A*
+echo '--------------------------'
+echo ''
